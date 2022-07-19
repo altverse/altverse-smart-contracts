3f47ccda026947c5382c37e39fe4c80bc7a358c5
Terry Kwon <terry@gixgames.com>
2022-07-19 15:43:47 +0900

    remove unnecessary codes

--- projects/escrow/scripts/deploy.ts
+++ projects/escrow/scripts/deploy.ts
@@ -1,12 +1,6 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
   const BaseContract = await ethers.getContractFactory("ArbitrableEscrowUpgradeable.sol");
   const baseContract = await BaseContract.deploy();
   await baseContract.deployed();
