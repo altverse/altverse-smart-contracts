f1f902325f3187da0440280a64c1a83047522d0a
fysoul17 <terry@gixgames.com>
2022-08-26 23:39:53 +0900

    apply upgradeable

--- projects/escrow/scripts/upgrade.ts
+++ projects/escrow/scripts/upgrade.ts
@@ -1,18 +1,21 @@
 import { ethers, upgrades } from "hardhat";
 import "@openzeppelin/hardhat-upgrades";
 
+const BASE_CONTRACT_ADDRESS = "";
+const FACTORY_CONTRACT_ADDRESS = "";
+
 async function main() {
   const BaseContract = await ethers.getContractFactory("ArbitrableEscrowUpgradeable");
-  const baseContract = await upgrades.deployProxy(BaseContract);
+  const baseContract = await upgrades.upgradeProxy(BASE_CONTRACT_ADDRESS, BaseContract);
   await baseContract.deployed();
 
-  console.log("Base contract deployed to:", baseContract.address);
+  console.log("Base contract UPGRADED to:", baseContract.address);
 
   const Factory = await ethers.getContractFactory("ArbitrableEscrowFactoryUpgradeable");
-  const facotry = await upgrades.deployProxy(Factory, [baseContract.address]);
+  const facotry = await upgrades.upgradeProxy(FACTORY_CONTRACT_ADDRESS, Factory);
   await facotry.deployed();
 
-  console.log("Factory contract deployed to:", facotry.address);
+  console.log("Factory contract UPGRADED to:", facotry.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
